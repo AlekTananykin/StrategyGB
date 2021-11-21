7b95f2e9a7c69cfc2fd0d17b326350dfcfecf038
AlekTananykin <tananykin@mail.ru>
2021-11-21 23:42:39 +0300

    selection is ready

--- Assets/Scripts/UserControlSystem/MouseInteractionsHandler.cs
+++ Assets/Scripts/UserControlSystem/MouseInteractionsHandler.cs
@@ -6,10 +6,12 @@ public class MouseInteractionsHandler : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
 
-
+    private Collider _selectedObjectCollider;
     
     void Update()
     {
+        ToOutLine();
+
         if (!Input.GetMouseButtonUp(0))
             return;
 
@@ -26,5 +28,28 @@ public class MouseInteractionsHandler : MonoBehaviour
             return;
 
         mainBuilding.ProduceUnit();
+    }
+
+    void ToOutLine()
+    {
+        if (!Physics.Raycast(
+            _camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+            return;
+
+        if (null != _selectedObjectCollider)
+        {
+            var prevObjectOutline = _selectedObjectCollider.GetComponent<Outline>();
+            if (null != prevObjectOutline)
+            {
+                prevObjectOutline.enabled = false;
+            }
+        }
+
+        var outlineComponent = hit.collider.GetComponent<Outline>();
+        if (null == outlineComponent)
+            return;
+
+        _selectedObjectCollider = hit.collider;
+        outlineComponent.enabled = true;
     }
 }
